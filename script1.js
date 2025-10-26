ec813b44e388ba5ced803f27df6018deea72ebd5
samian96 <sam.iannazzo@yahoo.com>
2025-10-25 17:38:44 -0700

    update 3 on readme / for while loop to add numbers / for while loop to subtract numbers

--- script1.js
+++ script1.js
@@ -6,4 +6,17 @@
 // ####
 // ###
 // ##
-// #
+// # 
+for (let i = "#"; i.length <= 4 ; i += "#") { // adds a # on every line until it is equal to 4
+    console.log (i);
+    while (i <= 4)
+        i +=1 
+}
+for (let i = "###"; i.length >= 1 ; i = i.slice(0, -1)) { // subtracts a # from every line until it is equal to 1
+    console.log (i);
+    while (i >= 1)
+        i -=1 
+}
+
+
+
